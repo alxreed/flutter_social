c8efc3852afc3f2484fdf91b72e749d1ff703fc1
Alex <ch.alexandre.reed@gmail.com>
2020-01-28 22:45:00 +0100

    added animation on the authentification buttons

--- lib/controllers/log_controller.dart
+++ lib/controllers/log_controller.dart
@@ -6,6 +6,21 @@ class LogController extends StatefulWidget {
 }
 
 class _LogState extends State<LogController> {
+  PageController _pageController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +31,43 @@ class _LogState extends State<LogController> {
         },
         child: SingleChildScrollView(
           child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: (MediaQuery.of(context).size.height >= 650.0)
-                ? MediaQuery.of(context).size.height
-                : 650.0,
-            decoration: MyGradient(startColor: base, endColor: baseAccent),
-          ),
+              width: MediaQuery.of(context).size.width,
+              height: (MediaQuery.of(context).size.height >= 650.0)
+                  ? MediaQuery.of(context).size.height
+                  : 650.0,
+              decoration: MyGradient(startColor: base, endColor: baseAccent),
+              child: SafeArea(
+                child: Column(
+                  children: <Widget>[
+                    PaddingWithChild(
+                        widget: Image(
+                      image: logoImage,
+                      height: 100.0,
+                    )),
+                    PaddingWithChild(
+                      widget: Menu2Items(
+                          item1: "Connexion",
+                          item2: "Création",
+                          pageController: _pageController),
+                      top: 20.0,
+                      bottom: 20.0,
+                    ),
+                    Expanded(
+                      child: PageView(
+                        controller: _pageController,
+                        children: <Widget>[logView(0), logView(1)],
+                      ),
+                      flex: 2,
+                    )
+                  ],
+                ),
+              )),
         ),
       ),
     );
+  }
+
+  Widget logView(int index) {
+    return Container(color: (index == 0) ? pointer : white);
   }
 }
---
+++ lib/views/my_widgets/padding_with_child.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PaddingWithChild extends Padding {
+
+  PaddingWithChild({
+    @required Widget widget,
+    double top: 10.0,
+    double left: 0.0,
+    double right: 0.0,
+    double bottom: 0.0
+  }): super(
+    padding: EdgeInsets.only(
+      top: top,
+      left: left,
+      right: right,
+      bottom: bottom
+    ),
+    child: widget
+  );
+
+  
+}
